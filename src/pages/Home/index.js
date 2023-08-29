2cfdd07881e778e250987331205d84627a9e8658
manoel <manoeldiasmf@gmail.com>
2023-08-28 22:01:24 -0300

    Update index.js

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,17 +1,27 @@
+import { useState } from 'react';
 import Banner from '../../components/Banner';
 import Category from '../../components/Category';
 import Container from '../../components/Container';
 import Footer from '../../components/Footer';
 import Header from '../../components/Header';
-import styles from './Home.module.css'
+import styles from './Home.module.css';
 
 function Home() {
+  let [nomeDigitado, setNomeDigitado] = useState("");
+
   return (
     <>
       <Header />
-      <Banner image={'Home'}/>
+      <Banner image={'Home'} />
       <Container>
-        <Category />
+        <input 
+        type="text" 
+        placeholder="Nome" 
+        onChange={(e) => setNomeDigitado(e.target.value)}
+        value={nomeDigitado}
+        />
+        <h2>{nomeDigitado}</h2>
+        {/* <Category /> */}
       </Container>
       <Footer />
     </>
@@ -19,4 +29,3 @@ function Home() {
 }
 
 export default Home;
-
